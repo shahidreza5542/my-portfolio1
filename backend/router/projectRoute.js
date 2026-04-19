c6fe10187fd16aace2cd86c82c5e6a7f9321d8d1
shahidreza <shahidreza660@gmail.com>
2026-04-19 05:37:13 +0530

    sd

--- backend/router/projectRoute.js
+++ backend/router/projectRoute.js
@@ -6,9 +6,6 @@ const projectRouter = express.Router()
 
 projectRouter.post('/add', addProject)
 projectRouter.post('/remove', removeProject)
-router.get("/", async (req, res) => {
-  const data = await ProjectModel.find()
-  res.json({ success: true, data })
-})
+
 
 export default projectRouter
